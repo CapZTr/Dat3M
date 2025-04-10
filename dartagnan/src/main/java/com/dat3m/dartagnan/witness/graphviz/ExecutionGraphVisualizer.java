740d7fbb92d46a45dde00704027757e42b08fa42
Tianrui Zheng <tianrui.zheng@huawei.com>
2025-04-10 16:49:47 +0200

    Add Alloc and MemFree to witness for debugging
    Signed-off-by: Tianrui Zheng <tianrui.zheng@huawei.com>

--- dartagnan/src/main/java/com/dat3m/dartagnan/witness/graphviz/ExecutionGraphVisualizer.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/witness/graphviz/ExecutionGraphVisualizer.java
@@ -106,7 +106,9 @@ public class ExecutionGraphVisualizer {
                  .filter(e -> e instanceof MemoryEventModel
                               || e instanceof GenericVisibleEventModel
                               || e instanceof LocalModel
-                              || e instanceof AssertModel)
+                              || e instanceof AssertModel
+                              || e instanceof AllocModel
+                              || e instanceof MemFreeModel)
                  .toList();
     }
 
